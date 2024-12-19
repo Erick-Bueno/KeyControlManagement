e9fbfe6d21e6be3ae8b89312be81c027dbcaa04c
Erick bueno <erickjb93@gmail.com>
2024-12-19 16:24:58 -0300

    feat(Authentication): add register command

---
+++ keycontrol/src/keycontrol.Application/Authentication/Commands/Register/RegisterCommand.cs
@@ -0,0 +1,9 @@
+﻿using keycontrol.Application.Authentication.Responses;
+using keycontrol.Application.Errors;
+using MediatR;
+using OneOf;
+using OneOf.Types;
+
+namespace keycontrol.Application.Authentication.Commands.Register;
+
+public record RegisterCommand(string Username, string Email, string Password) : IRequest<OneOf<RegisterResponse, AppError>>;
