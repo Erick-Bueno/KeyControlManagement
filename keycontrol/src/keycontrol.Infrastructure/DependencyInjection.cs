e153f8564dbbb29c6f6db19b72968228497f53ca
Erick bueno <erickjb93@gmail.com>
2024-12-19 17:16:47 -0300

    feat(DependencyInjection): add dependency injenction to bcrypt

--- keycontrol/src/keycontrol.Infrastructure/DependencyInjection.cs
+++ keycontrol/src/keycontrol.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using keycontrol.Application.Repositories;
+﻿using keycontrol.Application.Authentication.Common.Interfaces.Cryptography;
+using keycontrol.Application.Repositories;
+using keycontrol.Infrastructure.Cryptography;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace keycontrol.Infrastructure;
@@ -8,6 +10,7 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
         services.AddScoped<IUserRepository, IUserRepository>();
+        services.AddScoped<IBcrypt, Bcrypt>();
         return services;
     }
 }
