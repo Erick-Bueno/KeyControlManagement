80187e5693821e61dba00602e793bb705e4cc8c5
Erick bueno <erickjb93@gmail.com>
2024-12-19 16:37:59 -0300

    feat(Repository): add user repository and you abstraction

---
+++ keycontrol/src/keycontrol.Application/Repositories/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using keycontrol.Domain.Entities;
+
+namespace keycontrol.Application.Repositories;
+
+public interface IUserRepository
+{
+    public Task<User> FindUserByEmail(string email);
+    public Task<User> AddUser(User user);
+}
---
+++ keycontrol/src/keycontrol.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using keycontrol.Application.Repositories;
+using keycontrol.Domain.Entities;
+
+namespace keycontrol.Infrastructure.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    public Task<User> FindUserByEmail(string email)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> AddUser(User user)
+    {
+        throw new NotImplementedException();
+    }
+}
