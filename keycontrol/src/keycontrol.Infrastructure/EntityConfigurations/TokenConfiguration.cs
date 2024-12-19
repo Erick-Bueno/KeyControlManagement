b66739c673228314c9e73262e449cd430c02781b
Erick bueno <erickjb93@gmail.com>
2024-12-19 14:21:27 -0300

    feat(EntityConfiguration): add token configuration

---
+++ keycontrol/src/keycontrol.Infrastructure/EntityConfigurations/TokenConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using keycontrol.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace keycontrol.Infrastructure.EntityConfigurations;
+
+public class TokenConfiguration : IEntityTypeConfiguration<Token>
+{
+    public void Configure(EntityTypeBuilder<Token> builder)
+    {
+        builder.HasKey(t => t.Id);
+        builder.HasIndex(t => t.ExternalId);
+        builder.Property(t => t.Email).IsRequired().HasMaxLength(200);
+        builder.Property(t => t.RefreshToken).IsRequired().HasMaxLength(300);
+    }
+}
