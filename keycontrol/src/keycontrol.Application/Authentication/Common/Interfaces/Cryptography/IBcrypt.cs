55b9df69daa133320ae81c3067f91eaf9be414f4
Erick bueno <erickjb93@gmail.com>
2024-12-19 17:14:18 -0300

    feat(Crypthography): add bcrypt in project

---
+++ keycontrol/src/keycontrol.Application/Authentication/Common/Interfaces/Cryptography/IBcrypt.cs
@@ -0,0 +1,7 @@
+﻿namespace keycontrol.Application.Authentication.Common.Interfaces.Cryptography;
+
+public interface IBcrypt
+{
+    public string EncryptPassword(string password);
+    public bool DecryptPassword(string password, string encryptPassword);
+}
---
+++ keycontrol/src/keycontrol.Infrastructure/Cryptography/Bcrypt.cs
@@ -0,0 +1,16 @@
+﻿using keycontrol.Application.Authentication.Common.Interfaces.Cryptography;
+
+namespace keycontrol.Infrastructure.Cryptography;
+
+public class Bcrypt : IBcrypt
+{
+    public string EncryptPassword(string password)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(password);
+    }
+
+    public bool DecryptPassword(string password, string encryptPassword)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, encryptPassword);
+    }
+}
