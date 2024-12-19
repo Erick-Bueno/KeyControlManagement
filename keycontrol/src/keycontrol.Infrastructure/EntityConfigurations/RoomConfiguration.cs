dd2aa15d64da91038765c0075efde8c8e1158c8b
Erick bueno <erickjb93@gmail.com>
2024-12-19 14:18:59 -0300

    feat(EntityConfiguration): add room configuration

---
+++ keycontrol/src/keycontrol.Infrastructure/EntityConfigurations/RoomConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using keycontrol.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace keycontrol.Infrastructure.EntityConfigurations;
+
+public class RoomConfiguration : IEntityTypeConfiguration<Room>
+{
+    public void Configure(EntityTypeBuilder<Room> builder)
+    {
+        builder.HasKey(r => r.Id);
+        builder.HasIndex(r => r.ExternalId);
+        builder.Property(r => r.Name).IsRequired().HasMaxLength(200);
+    }
+}
