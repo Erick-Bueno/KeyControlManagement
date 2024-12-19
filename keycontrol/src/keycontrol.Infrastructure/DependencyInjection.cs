9ac952a75f8bbd658b77601a6b5e14c5fb43bbd9
Erick bueno <erickjb93@gmail.com>
2024-12-19 16:37:22 -0300

    feat(DependencyInjection): add dependency injection to infrastructure layer

---
+++ keycontrol/src/keycontrol.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using keycontrol.Application.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace keycontrol.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddScoped<IUserRepository, IUserRepository>();
+        return services;
+    }
+}
