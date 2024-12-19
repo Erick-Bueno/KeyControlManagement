3ec3c94e8a7907262669e304139de2450f9e1de9
Erick bueno <erickjb93@gmail.com>
2024-12-19 14:13:19 -0300

    feat(EntityConfiguration): add key configuration

---
+++ keycontrol/src/keycontrol.Infrastructure/EntityConfigurations/KeyConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using keycontrol.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace keycontrol.Infrastructure.EntityConfigurations;
+
+public class KeyConfiguration : IEntityTypeConfiguration<Key>
+{
+    public void Configure(EntityTypeBuilder<Key> builder)
+    {
+        builder.HasKey(k => k.Id);
+        builder.HasIndex(k => k.ExternalId);
+        builder.HasOne(k => k.Room)
+            .WithMany(r => r.Keys)
+            .HasForeignKey(k => k.IdRoom);
+    }
+}
