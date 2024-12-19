c184d9d76f1edb4eaeecf55749ea7fbe3479fd2d
Erick bueno <erickjb93@gmail.com>
2024-12-19 15:03:46 -0300

    feat(Dependencies): add necessay dependencies for connection with database

---
+++ keycontrol/src/keycontrol.Infrastructure/Context/AppDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace keycontrol.Infrastructure.Context;
+
+public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var basePath = Path.Combine(Directory.GetCurrentDirectory(), "..", "keycontrol.Api");
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(basePath)
+            .AddJsonFile("appsettings.Development.json")
+            .Build();
+        
+        var connectionString = configuration.GetConnectionString("Default");
+        var options = new DbContextOptionsBuilder<AppDbContext>();
+        options.UseNpgsql(connectionString, p => p.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName));
+        return new AppDbContext(options.Options);
+    }
+}
