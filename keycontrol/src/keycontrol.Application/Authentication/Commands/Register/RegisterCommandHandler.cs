6058a62fc6111d743586a42bce05f56910982920
Erick bueno <erickjb93@gmail.com>
2024-12-19 16:39:44 -0300

    feat(Command): add register command handler

---
+++ keycontrol/src/keycontrol.Application/Authentication/Commands/Register/RegisterCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using keycontrol.Application.Authentication.Responses;
+using keycontrol.Application.Errors;
+using MediatR;
+using OneOf;
+
+namespace keycontrol.Application.Authentication.Commands.Register;
+
+public class RegisterCommandHandler : IRequestHandler<RegisterCommand, OneOf<RegisterResponse, AppError>>
+{
+    public Task<OneOf<RegisterResponse, AppError>> Handle(RegisterCommand request, CancellationToken cancellationToken)
+    {
+        var userFinded = await _userRepository.FindUserByEmail(request.Email);
+
+        if (userFinded != null)
+        {
+            return new UserAlreadyRegistered("User already registered");
+        }
+        var encryptPassword = _bcryptNet.EncryptPassword(request.Password);
+
+        var newUser = User.CreateUser(request.Email, encryptPassword, request.UserName);
+
+        var user = await  _userRepository.AddUser(newUser);
+
+        return new RegisterResponse(user.Id, user.Username, user.Email);
+    }
+}
