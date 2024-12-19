5a40aefda8cf7da6ca5b88ee5c18b0fea48f83d9
Erick bueno <erickjb93@gmail.com>
2024-12-19 14:12:54 -0300

    feat(Entity): alter prop id to int

--- keycontrol/src/keycontrol.Domain/Entities/Entity.cs
+++ keycontrol/src/keycontrol.Domain/Entities/Entity.cs
@@ -2,7 +2,7 @@ namespace keycontrol.Domain.Entities;
 
 public abstract class Entity
 {
-    public Guid Id { get; protected set; }
+    public int Id { get; protected set; }
     public Guid ExternalId { get; protected set; } = Guid.NewGuid();
     public DateTime CreatedAt { get; protected set; }
     public DateTime UpdatedAt { get; protected set; }
