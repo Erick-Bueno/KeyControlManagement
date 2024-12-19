e055fcb27c0c501745f19c2e305ad243d7e30c14
Erick bueno <erickjb93@gmail.com>
2024-12-19 14:06:53 -0300

    feat(Dependencies): Add entity framework core

---
+++ keycontrol/src/keycontrol.Infrastructure/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using keycontrol.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace keycontrol.Infrastructure.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+    public DbSet<User> users { get; set; }
+    public DbSet<Key> keys { get; set; }
+    public DbSet<Report> reports { get; set; }
+    public DbSet<Token> tokens { get; set; }
+    public DbSet<Room> rooms { get; set; }
+}
