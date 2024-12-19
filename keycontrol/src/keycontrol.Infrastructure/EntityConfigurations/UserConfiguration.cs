5dfe5d10f93599f9feea23a786e09e65aac5ccdb
Erick bueno <erickjb93@gmail.com>
2024-12-19 14:09:02 -0300

    feat(EntityConfiguration): add user configuration

---
+++ keycontrol/src/keycontrol.Infrastructure/EntityConfigurations/UserConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using keycontrol.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace keycontrol.Infrastructure.EntityConfigurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(u => u.Id);
+        builder.HasIndex(u => u.ExternalId);
+        builder.Property(u => u.Name).IsRequired().HasMaxLength(200);
+        builder.Property(u => u.Email).IsRequired().HasMaxLength(200);
+        builder.Property(u => u.Password).IsRequired().HasMaxLength(400);
+        builder.Property(u => u.Iv).IsRequired().HasMaxLength(300);
+    }
+}
