64c9684c3b40e7a5bb78cefe8b5847f2467959bf
Erick bueno <erickjb93@gmail.com>
2024-12-19 17:54:00 -0300

    feat(Validators): add validator errors

---
+++ keycontrol/src/keycontrol.Application/Errors/Validators/ValidatorErrors.cs
@@ -0,0 +1,7 @@
+﻿using keycontrol.Domain.Enums;
+
+namespace keycontrol.Application.Errors.Validators;
+
+
+public record ValidatorErrors(List<ValidationError> errors)
+    : AppError("Validation errors" ,nameof(ValidatorErrors), TypeError.ValidationError.ToString(), errors);
