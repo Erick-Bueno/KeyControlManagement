cd9b1de7262b86ce2636abf37c369db5a4cc0cef
Erick Bueno <erickjb93@gmail.com>
2024-12-19 13:14:01 -0300

    feat: updated .gitignore

--- keycontrol/src/keycontrol.Domain/Entities/Entity.cs
+++
@@ -1,8 +0,0 @@
-namespace keycontrol.Domain.Entities;
-
-public abstract class Entity
-{
-    public Guid Id { get; protected set; }
-    public DateTime CreatedAt { get; protected set; }
-    public DateTime UpdatedAt { get; protected set; }
-}
--- keycontrol/src/keycontrol.Domain/Entities/Key.cs
+++
@@ -1,8 +0,0 @@
-namespace keycontrol.Domain.Entities;
-
-public class Key : Entity
-{   
-    public int IdRoom { get; private set; }
-    public Room Room { get; private set; }
-    public List<Report> Reports { get; private set; }
-}
