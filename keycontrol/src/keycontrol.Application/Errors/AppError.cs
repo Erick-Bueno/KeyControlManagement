9b7ce44d85189e9988b019141b996e94d6502895
Erick bueno <erickjb93@gmail.com>
2024-12-19 16:20:35 -0300

    feat(Error): add app error

---
+++ keycontrol/src/keycontrol.Application/Errors/AppError.cs
@@ -0,0 +1,5 @@
+﻿using keycontrol.Domain.Enums;
+
+namespace keycontrol.Application.Errors;
+
+public record AppError(string Detail, string NameError,  TypeError ErrorType, List<ValidationError>? ValidationErrors = null);
