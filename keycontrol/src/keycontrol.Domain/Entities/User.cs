819016603c220ce25e9a356b5d4f7af0b7638a51
Erick bueno <erickjb93@gmail.com>
2024-12-19 17:27:10 -0300

    fix(User): remove field iv

--- keycontrol/src/keycontrol.Domain/Entities/User.cs
+++ keycontrol/src/keycontrol.Domain/Entities/User.cs
@@ -2,25 +2,15 @@ namespace keycontrol.Domain.Entities;
 
 public class User : Entity
 {
-    public string Name { get; private set;}
-    public string Email { get; private set;}
-    public string Password { get; private set;}
-    public string Iv { get; private set;}
-    public List<Report> Reports { get; private set; }
-    public User SetName(string name){
+    public string Name { get; init;}
+    public string Email { get; init;}
+    public string Password { get; init;}
+    public List<Report> Reports { get; set; }
+
+    public User(string name, string email, string password)
+    {
         this.Name = name;
-        return this;
-    }
-    public User SetEmail(string email){
         this.Email = email;
-        return this;
-    }
-    public User SetPassword(string password){
         this.Password = password;
-        return this;
-    }
-    public User SetIv(string iv){
-        this.Iv = iv;
-        return this;
     }
 }
