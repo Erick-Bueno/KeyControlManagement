86e3c6f95add0ec423e4808fc732223927a0dee4
Erick Bueno <erickjb93@gmail.com>
2024-12-19 13:21:44 -0300

    feat: add report entity

---
+++ keycontrol/src/keycontrol.Domain/Entities/Report.cs
@@ -0,0 +1,15 @@
+namespace keycontrol.Domain.Entities;
+
+public class Report : Entity
+{
+    public int IdUser { get; private set; }
+    public int IdKey { get; private set; }
+    public Status Status { get; private set; }
+    public DateTime WithdrawalDate { get; private set; }
+    public DateTime? ReturnDate { get; private set; }
+    public Key Key { get; private set; }
+    public User User { get; private set; }
+    public Report(){
+        Status = Status.Available;
+    }
+}
