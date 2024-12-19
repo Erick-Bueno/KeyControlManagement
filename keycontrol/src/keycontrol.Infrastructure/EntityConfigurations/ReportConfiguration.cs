665ee679adb3377a18242118e607024d17ba5409
Erick bueno <erickjb93@gmail.com>
2024-12-19 14:16:01 -0300

    feat(EntityConfiguration): add report configuration

---
+++ keycontrol/src/keycontrol.Infrastructure/EntityConfigurations/ReportConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using keycontrol.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace keycontrol.Infrastructure.EntityConfigurations;
+
+public class ReportConfiguration : IEntityTypeConfiguration<Report>
+{
+    public void Configure(EntityTypeBuilder<Report> builder)
+    {
+        builder.HasKey(r => r.Id);
+        builder.HasIndex(r => r.ExternalId);
+        builder.Property(r => r.Status).IsRequired();
+        builder.Property(r => r.WithdrawalDate).IsRequired();
+        builder.HasOne(r => r.User)
+            .WithMany(u => u.Reports)
+            .HasForeignKey(r => r.IdUser);
+        builder.HasOne(r => r.Key)
+            .WithMany(k => k.Reports)
+            .HasForeignKey(r => r.IdKey);
+    }
+}
