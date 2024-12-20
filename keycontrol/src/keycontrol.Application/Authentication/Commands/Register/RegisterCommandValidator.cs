c29c4881fffecf0242c1b9ba9e67021ca1eac029
Erick bueno <erickjb93@gmail.com>
2024-12-20 15:43:51 -0300

    feat(Validation): add register command validator

---
+++ keycontrol/src/keycontrol.Application/Authentication/Commands/Register/RegisterCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace keycontrol.Application.Authentication.Commands.Register;
+
+public class RegisterCommandValidator : AbstractValidator<RegisterCommand>
+{
+    public RegisterCommandValidator()
+    {
+        RuleFor(r => r.Email)
+            .NotEmpty()
+            .Matches(@"^[^@\s]+@[^@\s]+\.[^@\s]+$").WithMessage("Invalid Email");
+        RuleFor(l => l.Password)
+            .NotEmpty()
+            .Matches(@"(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[-_@#$%]).{8,}").WithMessage("The password must contain at least 8 characters, a stored letter, a number and a special character");
+        RuleFor(r => r.Username)
+            .NotEmpty();
+    }
+}
