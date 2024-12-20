5dbbf46590d3c51318a18b7fb42b2a312dfd41d5
Erick bueno <erickjb93@gmail.com>
2024-12-20 15:43:25 -0300

    feat(Validation): add validation behavior

---
+++ keycontrol/src/keycontrol.Application/Behavior/ValidationBehavior.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using keycontrol.Application.Errors.Validators;
+using MediatR;
+using OneOf;
+
+namespace keycontrol.Application.Behavior;
+
+
+public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+    where TResponse : IOneOf
+{
+    private readonly IValidator<TRequest> _validator;
+
+    public ValidationBehavior(IValidator<TRequest> validator)
+    {
+        _validator = validator;
+    }
+
+    public async Task<TResponse> Handle(TRequest request,
+        RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var validationResult = await _validator.ValidateAsync(request);
+        if (validationResult.IsValid)
+        {
+            return await next();
+        }
+
+        var errors = validationResult.Errors.ConvertAll(validationFailure => new ValidationError()
+        {
+            Code = validationFailure.ErrorMessage,
+            Description = validationFailure.PropertyName
+        });
+        return (dynamic) new ValidatorErrors(errors);
+    }
+}
