c59a43e4efa007f5bfac28e9612b7dd300947ec0
Erick bueno <erickjb93@gmail.com>
2024-12-20 15:06:44 -0300

    feat(DependencyInjection): add appdbcontext dependency

--- keycontrol/src/keycontrol.Infrastructure/DependencyInjection.cs
+++ keycontrol/src/keycontrol.Infrastructure/DependencyInjection.cs
@@ -1,14 +1,18 @@
 ﻿using keycontrol.Application.Authentication.Common.Interfaces.Cryptography;
 using keycontrol.Application.Repositories;
+using keycontrol.Infrastructure.Context;
 using keycontrol.Infrastructure.Cryptography;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace keycontrol.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddDbContext<AppDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("default")));
         services.AddScoped<IUserRepository, IUserRepository>();
         services.AddScoped<IBcrypt, Bcrypt>();
         return services;
