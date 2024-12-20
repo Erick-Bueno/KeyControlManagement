21ba3110568e0f7d7a0358d714e2d7a6fdb06ebb
Erick bueno <erickjb93@gmail.com>
2024-12-20 15:44:51 -0300

    test(Tests): add register command handle test given user already exists then return user already registered error

---
+++ keycontrol/src/keycontrol.Tests/Authentication/Commands/Register/RegisterCommandHandlerTests.cs
@@ -0,0 +1,42 @@
+﻿using Bogus;
+using FluentAssertions;
+using keycontrol.Application.Authentication.Commands.Register;
+using keycontrol.Application.Authentication.Common.Interfaces.Cryptography;
+using keycontrol.Application.Errors;
+using keycontrol.Application.Repositories;
+using keycontrol.Domain.Entities;
+using Moq;
+using Xunit;
+
+namespace keycontrol.Tests.Authentication.Commands.Register;
+
+public class RegisterCommandHandlerTests
+{
+    private readonly RegisterCommandHandler _registerCommandHandler;
+    private readonly Mock<IUserRepository> _userRepositoryMock;
+    private readonly Faker _faker = new Faker("pt_BR");
+    public RegisterCommandHandlerTests()
+    {
+        _userRepositoryMock = new Mock<IUserRepository>();
+        var bcryptMock = new Mock<IBcrypt>();
+        _registerCommandHandler = new RegisterCommandHandler(_userRepositoryMock.Object, bcryptMock.Object);
+    }
+    [Fact]
+    [Trait("Category", "RegisterCommandHandler")]
+    public async void Handle_GivenUserAlreadyExists_ThenReturnUserAlreadyRegisteredError()
+    {
+        var userAlreadyRegistered = new User(_faker.Person.FirstName, _faker.Person.Email, _faker.Random.AlphaNumeric(8));
+        var user = _userRepositoryMock.Setup(ur => ur.FindUserByEmail(userAlreadyRegistered.Email))
+            .ReturnsAsync(userAlreadyRegistered);
+        var request =
+            new RegisterCommand(userAlreadyRegistered.Name, userAlreadyRegistered.Email,
+                userAlreadyRegistered.Password);
+        var expectedResponseError = new UserAlreadyRegistered("User already registered");
+
+        var result = await _registerCommandHandler.Handle(request, CancellationToken.None);
+
+        result.AsT1.ErrorType.Should().Be(expectedResponseError.ErrorType);
+        result.AsT1.NameError.Should().Be(expectedResponseError.NameError);
+        result.AsT1.Detail.Should().Be(expectedResponseError.Detail);
+    }
+}
