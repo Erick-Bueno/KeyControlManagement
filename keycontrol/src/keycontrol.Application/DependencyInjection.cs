6a3f11976fe6fe1154a606fc6b6742aec5366459
Erick bueno <erickjb93@gmail.com>
2024-12-20 15:00:39 -0300

    feat(DependencyInjection): add dependency injection class in application layer

---
+++ keycontrol/src/keycontrol.Application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using FluentValidation;
+using keycontrol.Application.Behavior;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace keycontrol.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+        services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>)); //registrar o pipeline de validação que e executado antes de chamar o handler
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly()); //registrar os validadores
+        return services;
+    }
+}
