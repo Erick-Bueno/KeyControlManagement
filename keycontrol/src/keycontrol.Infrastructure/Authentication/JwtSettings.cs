3605b5450af9fe3b0f86346879194767b7571025
Erick Bueno <erickjb93@gmail.com>
2024-12-22 13:48:28 -0300

    feat: add class to jwt settings map

---
+++ keycontrol/src/keycontrol.Infrastructure/Authentication/JwtSettings.cs
@@ -0,0 +1,11 @@
+namespace keycontrol.Infrastructure.Authentication;
+
+public class JwtSettings
+{
+    public const string SectionName = "JwtSettings";
+    public string Secretkey { get; set; }
+    public string Issuer { get; set; }
+    public string Audience { get; set; }
+    public int ExpiresInAccessToken { get; set; }
+    public int ExpiresInRefreshToken { get; set; }
+}
