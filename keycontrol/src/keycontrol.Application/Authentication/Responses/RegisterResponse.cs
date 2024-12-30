29f763c034ef4cfe5c458164b409f83e8badaee3
Erick Bueno <erickjb93@gmail.com>
2024-12-30 18:45:03 -0300

    fix: fix type prop external id to guid

--- keycontrol/src/keycontrol.Application/Authentication/Responses/RegisterResponse.cs
+++ keycontrol/src/keycontrol.Application/Authentication/Responses/RegisterResponse.cs
@@ -1,3 +1,3 @@
 ﻿namespace keycontrol.Application.Authentication.Responses;
 
-public record RegisterResponse(string ExternalId, string Name, string Email);
+public record RegisterResponse(Guid ExternalId, string Name, string Email);
