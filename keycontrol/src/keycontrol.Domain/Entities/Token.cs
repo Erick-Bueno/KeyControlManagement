15c3f39c911e0a5b817c5e77d5379df3d481835d
Erick Bueno <erickjb93@gmail.com>
2024-12-30 18:30:39 -0300

    feat: add update token method

--- keycontrol/src/keycontrol.Domain/Entities/Token.cs
+++ keycontrol/src/keycontrol.Domain/Entities/Token.cs
@@ -2,6 +2,17 @@ namespace keycontrol.Domain.Entities;
 
 public class Token : Entity
 {
+    public Token(string email, string refreshToken)
+    {
+        Email = email;
+        RefreshToken = refreshToken;
+    }
+
     public string Email { get; private set; }
     public string RefreshToken { get; private set; }
+
+    public static Token UpdateToken(Token token, string refreshToken){
+        token.RefreshToken = refreshToken;
+        return token;
+    }
 }
