99c20c6bba57195fb58e7630507f91a75aeb407f
Erick Bueno <erickjb93@gmail.com>
2024-12-30 18:16:45 -0300

    feat: implement token repository methods

---
+++ keycontrol/src/keycontrol.Infrastructure/Repositories/TokenRepository.cs
@@ -0,0 +1,34 @@
+using keycontrol.Application.Repositories;
+using keycontrol.Domain.Entities;
+using keycontrol.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace keycontrol.Infrastructure.Repositories
+{
+    public class TokenRepository : ITokenRepository
+    {
+        private readonly AppDbContext _context;
+
+        public TokenRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Token> FindTokenByEmail(string email)
+        {
+            return await _context.tokens.Where(t => t.Email == email).FirstOrDefaultAsync();
+        }
+
+        public async Task AddToken(Token token)
+        {
+            await _context.tokens.AddAsync(token);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateToken(Token token, string newRefreshToken)
+        {
+            Token.UpdateToken(token, newRefreshToken);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
