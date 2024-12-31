170f49f0b6d4b3a5adfa7b38dbe223f2bb2aed05
Erick bueno <erickjb93@gmail.com>
2024-12-31 09:43:52 -0300

    test: add given user not registered then return user not registered error test

---
+++ keycontrol/src/keycontrol.Tests/Authentication/Queries/Login/LoginQueryHandlerTests.cs
@@ -0,0 +1,43 @@
+using Bogus;
+using FluentAssertions;
+using keycontrol.Application.Authentication.Common.Interfaces.Authentication;
+using keycontrol.Application.Authentication.Common.Interfaces.Cryptography;
+using keycontrol.Application.Authentication.Queries.Login;
+using keycontrol.Application.Errors;
+using keycontrol.Application.Repositories;
+using keycontrol.Domain.Entities;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Moq;
+using Xunit;
+
+public class LoginQueryHandlerTests
+{
+    private readonly Mock<IUserRepository> _userRepositoryMock;
+    private readonly LoginQueryHandler _loginQueryHandler;
+    private readonly Faker _faker = new Faker("pt_BR");
+    public LoginQueryHandlerTests(){
+        _userRepositoryMock = new Mock<IUserRepository>();
+        var _bcrypt = new Mock<IBcrypt>();
+        var _tokenJwtGenerator = new Mock<ITokenJwtGenerator>();
+        var _tokenRepository = new Mock<ITokenRepository>(); 
+
+        _loginQueryHandler = new LoginQueryHandler(_userRepositoryMock.Object, _bcrypt.Object, _tokenJwtGenerator.Object, _tokenRepository.Object);
+    }
+
+    [Fact]
+    [Trait("Category", "LoginQueryHandler")]
+    public async void Handle_GivenUserNotRegistered_ThenReturnUserNotRegisteredError()
+    {
+        var emailNotRegistered = _faker.Person.Email;
+        var loginQuery = new LoginQuery(emailNotRegistered, _faker.Random.AlphaNumeric(8));
+        _userRepositoryMock.Setup(ur => ur.FindUserByEmail(emailNotRegistered)).ReturnsAsync((User)null);
+
+        var result = await _loginQueryHandler.Handle(loginQuery, CancellationToken.None);
+
+        var expectedResponseError = new UserNotRegistered("User not registered");
+
+        result.AsT1.ErrorType.Should().Be(expectedResponseError.ErrorType);
+        result.AsT1.Detail.Should().Be(expectedResponseError.Detail);
+        result.AsT1.NameError.Should().Be(expectedResponseError.NameError);
+    }
+}
