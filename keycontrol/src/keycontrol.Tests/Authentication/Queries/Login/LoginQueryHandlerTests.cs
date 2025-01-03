e025bf8874506d6b64f59e6f42c811cf50c3383d
Erick bueno <erickjb93@gmail.com>
2025-01-03 15:52:36 -0300

    test(Test): add given token not nul then update token and login

--- keycontrol/src/keycontrol.Tests/Authentication/Queries/Login/LoginQueryHandlerTests.cs
+++ keycontrol/src/keycontrol.Tests/Authentication/Queries/Login/LoginQueryHandlerTests.cs
@@ -3,6 +3,7 @@ using FluentAssertions;
 using keycontrol.Application.Authentication.Common.Interfaces.Authentication;
 using keycontrol.Application.Authentication.Common.Interfaces.Cryptography;
 using keycontrol.Application.Authentication.Queries.Login;
+using keycontrol.Application.Authentication.Responses;
 using keycontrol.Application.Errors;
 using keycontrol.Application.Repositories;
 using keycontrol.Domain.Entities;
@@ -15,12 +16,14 @@ public class LoginQueryHandlerTests
     private readonly Mock<IUserRepository> _userRepositoryMock;
     private readonly Mock<IBcrypt> _bcrypt;
     private readonly LoginQueryHandler _loginQueryHandler;
+    private readonly Mock<ITokenRepository> _tokenRepository;
+    private readonly Mock<ITokenJwtGenerator> _tokenJwtGenerator;
     private readonly Faker _faker = new Faker("pt_BR");
     public LoginQueryHandlerTests(){
         _userRepositoryMock = new Mock<IUserRepository>();
         _bcrypt = new Mock<IBcrypt>();
-        var _tokenJwtGenerator = new Mock<ITokenJwtGenerator>();
-        var _tokenRepository = new Mock<ITokenRepository>(); 
+        _tokenJwtGenerator = new Mock<ITokenJwtGenerator>();
+        _tokenRepository = new Mock<ITokenRepository>(); 
 
         _loginQueryHandler = new LoginQueryHandler(_userRepositoryMock.Object, _bcrypt.Object, _tokenJwtGenerator.Object, _tokenRepository.Object);
     }
@@ -61,4 +64,33 @@ public class LoginQueryHandlerTests
         result.AsT1.Detail.Should().Be(expectedResponseError.Detail);
         result.AsT1.NameError.Should().Be(expectedResponseError.NameError);
     }
+
+    [Fact]
+    [Trait("Category", "LoginQueryHandler")]
+    public async Task Handle_GivenTokenIsNotNull_ThenUpdateTokenAndLoginAsync()
+    {
+        //arrumar esse teste
+        var loginQuery = new LoginQuery(_faker.Person.Email, _faker.Random.AlphaNumeric(8));
+        var user = new User(_faker.Person.FirstName, loginQuery.Email, loginQuery.Password);
+        var token = new Token(user.Email, _faker.Random.AlphaNumeric(18));
+        var accessToken = _faker.Random.AlphaNumeric(8);
+        var refreshToken = _faker.Random.AlphaNumeric(8);
+        _userRepositoryMock.Setup(ur => ur.FindUserByEmail(loginQuery.Email)).ReturnsAsync(user);
+        _bcrypt.Setup(b => b.VerifyPassword(loginQuery.Password, user.Password)).Returns(true);
+        _tokenRepository.Setup(t => t.FindTokenByEmail(user.Email)).ReturnsAsync(token);
+        _tokenJwtGenerator.Setup(t => t.GenerateAccessToken(user.ExternalId)).Returns(accessToken);
+        _tokenJwtGenerator.Setup(t => t.GenerateRefreshToken()).Returns(refreshToken);
+        
+        var result = await _loginQueryHandler.Handle(loginQuery, CancellationToken.None);
+        
+        var expectedResponseSuccess= new LoginResponse(user.ExternalId, user.Name, user.Email, accessToken, refreshToken);
+        
+        _tokenRepository.Verify(t => t.UpdateToken(token, token.RefreshToken), Times.Once);
+        
+        result.AsT0.Email.Should().Be(expectedResponseSuccess.Email);
+        result.AsT0.Name.Should().Be(expectedResponseSuccess.Name);
+        result.AsT0.ExternalId.Should().Be(expectedResponseSuccess.ExternalId);
+        result.AsT0.AccessToken.Should().Be(expectedResponseSuccess.AccessToken);
+        result.AsT0.RefreshToken.Should().Be(expectedResponseSuccess.RefreshToken);
+    }   
 }
