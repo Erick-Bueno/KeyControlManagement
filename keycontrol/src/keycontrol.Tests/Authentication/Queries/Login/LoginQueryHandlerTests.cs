57f2d5122d48a2b437be4130b66816dbb9b1b608
Erick bueno <erickjb93@gmail.com>
2025-01-03 15:14:12 -0300

    ✅ Adding Tests: add given invalid password then return invalid password error test

--- keycontrol/src/keycontrol.Tests/Authentication/Queries/Login/LoginQueryHandlerTests.cs
+++ keycontrol/src/keycontrol.Tests/Authentication/Queries/Login/LoginQueryHandlerTests.cs
@@ -13,11 +13,12 @@ using Xunit;
 public class LoginQueryHandlerTests
 {
     private readonly Mock<IUserRepository> _userRepositoryMock;
+    private readonly Mock<IBcrypt> _bcrypt;
     private readonly LoginQueryHandler _loginQueryHandler;
     private readonly Faker _faker = new Faker("pt_BR");
     public LoginQueryHandlerTests(){
         _userRepositoryMock = new Mock<IUserRepository>();
-        var _bcrypt = new Mock<IBcrypt>();
+        _bcrypt = new Mock<IBcrypt>();
         var _tokenJwtGenerator = new Mock<ITokenJwtGenerator>();
         var _tokenRepository = new Mock<ITokenRepository>(); 
 
@@ -26,7 +27,7 @@ public class LoginQueryHandlerTests
 
     [Fact]
     [Trait("Category", "LoginQueryHandler")]
-    public async void Handle_GivenUserNotRegistered_ThenReturnUserNotRegisteredError()
+    public async Task Handle_GivenUserNotRegistered_ThenReturnUserNotRegisteredErrorAsync()
     {
         var emailNotRegistered = _faker.Person.Email;
         var loginQuery = new LoginQuery(emailNotRegistered, _faker.Random.AlphaNumeric(8));
@@ -35,6 +36,26 @@ public class LoginQueryHandlerTests
         var result = await _loginQueryHandler.Handle(loginQuery, CancellationToken.None);
 
         var expectedResponseError = new UserNotRegistered("User not registered");
+
+        result.AsT1.ErrorType.Should().Be(expectedResponseError.ErrorType);
+        result.AsT1.Detail.Should().Be(expectedResponseError.Detail);
+        result.AsT1.NameError.Should().Be(expectedResponseError.NameError);
+    }
+
+    [Fact]
+    [Trait("Category", "LoginQueryHandler")]	
+    public async Task Handle_GivenInvalidPassword_ThenReturnInvalidPasswordAsync()
+    {
+        var invalidPassword = _faker.Random.AlphaNumeric(8);
+        var correctPassword = _faker.Random.AlphaNumeric(8);
+        var loginQuery = new LoginQuery(_faker.Person.Email, invalidPassword);
+        var user = new User(_faker.Person.FirstName, loginQuery.Email, correctPassword);
+        _userRepositoryMock.Setup(ur => ur.FindUserByEmail(loginQuery.Email)).ReturnsAsync(user);
+        _bcrypt.Setup(b => b.VerifyPassword(invalidPassword, user.Password)).Returns(false);
+
+        var result = await _loginQueryHandler.Handle(loginQuery, CancellationToken.None);
+
+        var expectedResponseError = new InvalidPassword("Invalid password");
 
         result.AsT1.ErrorType.Should().Be(expectedResponseError.ErrorType);
         result.AsT1.Detail.Should().Be(expectedResponseError.Detail);
