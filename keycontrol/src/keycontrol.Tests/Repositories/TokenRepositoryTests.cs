32f9954a03d61ff3dc88fb7ff6e3fbfb2229ea3b
Erick Bueno <erickjb93@gmail.com>
2025-01-05 12:04:25 -0300

    feat: add given token then add token async test

--- keycontrol/src/keycontrol.Tests/Repositories/TokenRepositoryTests.cs
+++ keycontrol/src/keycontrol.Tests/Repositories/TokenRepositoryTests.cs
@@ -11,7 +11,7 @@ namespace keycontrol.Tests.Repositories;
 
 public class TokenRepositoryTests
 {
-    private readonly AppDbContext  _dbContext;
+    private readonly AppDbContext _dbContext;
     private readonly ITokenRepository _tokenRepository;
     private readonly Faker _faker = new Faker("pt_BR");
 
@@ -33,29 +33,44 @@ public class TokenRepositoryTests
     [Trait("Category", "TokenRepository")]
     public async Task FindTokenByEmail__GivenNotRegisteredEmail_ThenReturnNullAsync()
     {
-       var token = new Token(_faker.Person.Email, _faker.Random.AlphaNumeric(8));
-       var unregisteredEmail = _faker.Person.Email + "unregistered";
-       _dbContext.tokens.Add(token);
-       await _dbContext.SaveChangesAsync();
+        var token = new Token(_faker.Person.Email, _faker.Random.AlphaNumeric(8));
+        var unregisteredEmail = _faker.Person.Email + "unregistered";
+        _dbContext.tokens.Add(token);
+        await _dbContext.SaveChangesAsync();
 
-       var result = await _tokenRepository.FindTokenByEmail(unregisteredEmail);
+        var result = await _tokenRepository.FindTokenByEmail(unregisteredEmail);
 
-       result.Should().BeNull();
+        result.Should().BeNull();
     }
     [Fact]
     [Trait("Category", "TokenRepository")]
     public async Task FindTokenByEmail_GivenRegisteredEmail_ThenReturnTokenAsync()
     {
-       var registeredEmail = _faker.Person.Email;
-       var token = new Token(registeredEmail, _faker.Random.AlphaNumeric(8));
-       _dbContext.tokens.Add(token);
-       await _dbContext.SaveChangesAsync();
+        var registeredEmail = _faker.Person.Email;
+        var token = new Token(registeredEmail, _faker.Random.AlphaNumeric(8));
+        _dbContext.tokens.Add(token);
+        await _dbContext.SaveChangesAsync();
 
-       var result = await _tokenRepository.FindTokenByEmail(registeredEmail);
+        var result = await _tokenRepository.FindTokenByEmail(registeredEmail);
 
-       result.Email.Should().Be(token.Email);
-       result.ExternalId.Should().Be(token.ExternalId);
-       result.Id.Should().Be(token.Id);
-       result.RefreshToken.Should().Be(token.RefreshToken);
+        result.Email.Should().Be(token.Email);
+        result.ExternalId.Should().Be(token.ExternalId);
+        result.Id.Should().Be(token.Id);
+        result.RefreshToken.Should().Be(token.RefreshToken);
+    }
+    [Fact]
+    [Trait("Category", "TokenRepository")]
+    public async Task AddToken_GivenToken_ThenAddTokenAsync()
+    {
+        var token = new Token(_faker.Person.Email, _faker.Random.AlphaNumeric(8));
+
+        await _tokenRepository.AddToken(token);
+
+        var tokenFinded = await _dbContext.tokens.FindAsync(token.Id);
+        tokenFinded.Should().NotBeNull();
+        tokenFinded.Email.Should().Be(token.Email);
+        tokenFinded.ExternalId.Should().Be(token.ExternalId);
+        tokenFinded.Id.Should().Be(token.Id);
+        tokenFinded.RefreshToken.Should().Be(token.RefreshToken);
     }
 }
