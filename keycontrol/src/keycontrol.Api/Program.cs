f58bb3d871f4edc8bbbfcb4b9eac63b929324699
Erick bueno <erickjb93@gmail.com>
2025-01-13 16:39:22 -0300

    feat: add in memory cache configuration

--- keycontrol/src/keycontrol.Api/Program.cs
+++ keycontrol/src/keycontrol.Api/Program.cs
@@ -10,6 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddMemoryCache();
+//adicionar cache distribuido com redis
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
 builder.Services.AddCors(options => {
