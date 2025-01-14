682007ab42134413cccafdeaa6b8d3cf0d17728e
Erick bueno <erickjb93@gmail.com>
2025-01-14 11:33:04 -0300

    feat(Errors): Add room not finded error

---
+++ keycontrol/src/keycontrol.Application/Errors/RoomNotFinded.cs
@@ -0,0 +1,5 @@
+﻿using keycontrol.Domain.Enums;
+
+namespace keycontrol.Application.Errors;
+
+public record RoomNotFinded(string Detail):AppError(Detail, nameof(RoomNotFinded), TypeError.Conflict.ToString());
