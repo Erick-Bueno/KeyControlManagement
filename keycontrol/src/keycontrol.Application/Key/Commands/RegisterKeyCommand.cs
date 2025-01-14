41e4f637ed75a621e7dad13f2cdf3995fa21eb86
Erick bueno <erickjb93@gmail.com>
2025-01-14 11:45:26 -0300

    feat(Command): add register key command

---
+++ keycontrol/src/keycontrol.Application/Key/Commands/RegisterKeyCommand.cs
@@ -0,0 +1,8 @@
+﻿using keycontrol.Application.Errors;
+using keycontrol.Application.Key.Responses;
+using MediatR;
+using OneOf;
+
+namespace keycontrol.Application.Key.Commands;
+
+public record RegisterKeyCommand(Guid ExternalIdRoom, string Description) : IRequest<OneOf<RegisterKeyResponse, AppError>>;
