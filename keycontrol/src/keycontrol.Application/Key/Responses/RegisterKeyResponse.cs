e6b8e5ee2c941ebd8392bf8480d8fa6f07ef2f64
Erick bueno <erickjb93@gmail.com>
2025-01-14 11:33:20 -0300

    feat(Responses): add register key response

---
+++ keycontrol/src/keycontrol.Application/Key/Responses/RegisterKeyResponse.cs
@@ -0,0 +1,4 @@
+﻿namespace keycontrol.Application.Key.Responses;
+
+public record RegisterKeyResponse(Guid ExternalId, Guid ExternalIdRoom, string Description, string Room)
+    : GlobalResponse("Key registered successfully");
