6f33215dc5aa9c32eec9732adb607ff921714eaa
Erick bueno <erickjb93@gmail.com>
2025-01-14 11:46:55 -0300

    feat(Entity): add set in key room entity

--- keycontrol/src/keycontrol.Domain/Entities/KeyRoom.cs
+++ keycontrol/src/keycontrol.Domain/Entities/KeyRoom.cs
@@ -4,14 +4,14 @@ namespace keycontrol.Domain.Entities;
 
 public class KeyRoom : Entity
 {   
-    public int IdRoom { get; }
-    public string? Description { get; }
+    public int IdRoom { get; init; }
+    public string? Description { get; init; }
     public Room? Room { get; }
     public Collection<Report>? Reports { get; }
-
+    
     public KeyRoom(int idRoom, string? description)
     {
-        this.IdRoom = idRoom; 
-        this.Description = description;
+        IdRoom = idRoom; 
+        Description = description;
     }
 }
