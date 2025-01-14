cfd8831f1d3f4fbe696df8a1c0008aef48b5626f
Erick bueno <erickjb93@gmail.com>
2025-01-14 11:47:20 -0300

    feat(Repository): add key repository

---
+++ keycontrol/src/keycontrol.Infrastructure/Repositories/KeyRepository.cs
@@ -0,0 +1,21 @@
+﻿using keycontrol.Application.Repositories;
+using keycontrol.Domain.Entities;
+using keycontrol.Infrastructure.Context;
+
+namespace keycontrol.Infrastructure.Repositories;
+
+public class KeyRepository : IKeyRepository
+{
+    private readonly AppDbContext _context;
+
+    public KeyRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddKey(KeyRoom key)
+    {
+        await _context.keys.AddAsync(key);
+        await _context.SaveChangesAsync();
+    }
+}
