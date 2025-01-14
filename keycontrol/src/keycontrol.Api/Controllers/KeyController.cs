ab1a4911537f37afe91abb71dae10452b7345fc5
Erick bueno <erickjb93@gmail.com>
2025-01-14 11:47:47 -0300

    feat(Controller): add key controller

---
+++ keycontrol/src/keycontrol.Api/Controllers/KeyController.cs
@@ -0,0 +1,24 @@
+﻿using keycontrol.Application.Extension;
+using keycontrol.Application.Key.Commands;
+using keycontrol.Application.Key.Requests;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace keycontrol.Api.Controllers;
+
+public class KeyController : ApiController
+{
+    private readonly ISender _sender;
+    public KeyController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddKey([FromBody] RegisterKeyRequest registerKeyRequest)
+    {
+        var registerKeyCommand = new RegisterKeyCommand(registerKeyRequest.ExternalIdRoom, registerKeyRequest.Description);
+        var result = await _sender.Send(registerKeyCommand);
+        return this.RegisterKeyResponseBase(result);
+    }
+}
