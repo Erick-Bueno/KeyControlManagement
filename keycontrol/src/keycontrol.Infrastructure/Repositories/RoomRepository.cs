f5e341e6537b14750e9c392c9f6c1b934d60a176
Erick bueno <erickjb93@gmail.com>
2025-01-14 11:32:43 -0300

    feat(Repository): add room repository

---
+++ keycontrol/src/keycontrol.Infrastructure/Repositories/RoomRepository.cs
@@ -0,0 +1,21 @@
+﻿using keycontrol.Application.Repositories;
+using keycontrol.Domain.Entities;
+using keycontrol.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace keycontrol.Infrastructure.Repositories;
+
+public class RoomRepository : IRoomRepository
+{
+    private readonly AppDbContext _context;
+
+    public RoomRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Room> GetRoomByExternalId(Guid externalId)
+    {
+        return await _context.rooms.Where(r => r.ExternalId.Equals(externalId)).FirstOrDefaultAsync();
+    }
+}
