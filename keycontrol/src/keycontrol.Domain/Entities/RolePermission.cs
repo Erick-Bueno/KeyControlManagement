a487e16754744d001580791a2162cb75b34c0d60
Erick bueno <erickjb93@gmail.com>
2025-01-15 11:07:09 -0300

    feat: add role permission

--- keycontrol/src/keycontrol.Domain/Entities/RolePermission.cs
+++ keycontrol/src/keycontrol.Domain/Entities/RolePermission.cs
@@ -1,7 +1,7 @@
-﻿namespace keycontrol.Domain.Entities
+﻿namespace keycontrol.Domain.Entities;
+
+public class RolePermission : Entity
 {
-    public class RolePermission
-    {
-        
-    }
+    public int RoleId { get; set; }
+    public int PermissionId { get; set; }
 }
