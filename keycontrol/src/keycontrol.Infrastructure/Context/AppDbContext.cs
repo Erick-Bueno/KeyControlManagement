f58a646c15fd8d6a23ca8765ad580fd7e7534aea
Erick bueno <erickjb93@gmail.com>
2025-01-15 16:19:15 -0300

    refactor: refact apply configuration

--- keycontrol/src/keycontrol.Infrastructure/Context/AppDbContext.cs
+++ keycontrol/src/keycontrol.Infrastructure/Context/AppDbContext.cs
@@ -3,16 +3,15 @@ using keycontrol.Infrastructure.EntityConfigurations;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
+using System.Reflection;
 
 namespace keycontrol.Infrastructure.Context;
 
 public class AppDbContext : DbContext
 {
-    private readonly IConfiguration _configuration;
 
-    public AppDbContext(DbContextOptions options, IConfiguration configuration) : base(options)
+    public AppDbContext(DbContextOptions options) : base(options)
     {
-        _configuration = configuration;
     }
 
     public DbSet<User> users { get; set; }
@@ -26,44 +25,52 @@ public class AppDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.ApplyConfiguration(new UserConfiguration());
-        modelBuilder.ApplyConfiguration(new TokenConfiguration());
-        modelBuilder.ApplyConfiguration(new RoomConfiguration());
-        modelBuilder.ApplyConfiguration(new KeyConfiguration());
-        modelBuilder.ApplyConfiguration(new ReportConfiguration());
-        modelBuilder.ApplyConfiguration(new PermissionConfiguration());
-        modelBuilder.ApplyConfiguration(new RoleConfiguration());
-        modelBuilder.ApplyConfiguration(new RolePermissionConfiguration());
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
     }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        if (!AppDbContextFactory.IsMigrationContext){
+        if (!AppDbContextFactory.IsMigrationContext)
+        {
             var permissionsEnum = Enum.GetValues<Domain.Enums.Permission>()
             .Where(p => p != Domain.Enums.Permission.None)
             .Select(p => new Permission { Id = (int)p, Name = p.ToString() });
-        var rolePermissions = new[]
-        {
+            var rolePermissions = new[]
+            {
             new RolePermission() { RoleId = Role.Doorman.Id, PermissionId = (int)Domain.Enums.Permission.Administrator },
             new RolePermission() { RoleId = Role.CommonUser.Id, PermissionId = (int)Domain.Enums.Permission.ReadMember }
         };
 
-        optionsBuilder.UseNpgsql(_configuration.GetConnectionString("Default"))
-            .UseAsyncSeeding(async (context, _, cancellationToken) =>
-            {
-                await context.Set<Permission>()
-                    .ExecuteDeleteAsync(cancellationToken);
-                await context.Set<Role>()
-                    .ExecuteDeleteAsync(cancellationToken);
-                await context.Set<RolePermission>()
-                    .ExecuteDeleteAsync(cancellationToken);
+            optionsBuilder
+                .UseSeeding((context, _) => {
+                    context.Set<Permission>()
+                        .ExecuteDelete();
+                    context.Set<Role>()
+                        .ExecuteDelete();
+                    context.Set<RolePermission>()
+                        .ExecuteDelete();
 
-                await context.Set<Permission>().AddRangeAsync(permissionsEnum, cancellationToken);
-                await context.Set<Role>().AddRangeAsync(Role.GetValues(), cancellationToken);
-                await context.Set<RolePermission>().AddRangeAsync(rolePermissions, cancellationToken);
-                
-                await context.SaveChangesAsync(cancellationToken);
-            });
+                    context.Set<Permission>().AddRange(permissionsEnum);
+                    context.Set<Role>().AddRange(Role.GetValues());
+                    context.Set<RolePermission>().AddRange(rolePermissions);
+
+                    context.SaveChanges();
+                })
+                .UseAsyncSeeding(async (context, _, cancellationToken) =>
+                {
+                    await context.Set<Permission>()
+                        .ExecuteDeleteAsync(cancellationToken);
+                    await context.Set<Role>()
+                        .ExecuteDeleteAsync(cancellationToken);
+                    await context.Set<RolePermission>()
+                        .ExecuteDeleteAsync(cancellationToken);
+
+                    context.Set<Permission>().AddRange(permissionsEnum);
+                    context.Set<Role>().AddRange(Role.GetValues());
+                    context.Set<RolePermission>().AddRange(rolePermissions);
+
+                    await context.SaveChangesAsync(cancellationToken);
+                });
         }
-        
+
     }
 }
