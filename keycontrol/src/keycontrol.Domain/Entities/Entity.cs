9271dfbb77535746d0ca13b68b0e3a1760ec56e4
Erick bueno <erickjb93@gmail.com>
2025-01-15 10:57:00 -0300

    fix: alter external id generated

--- keycontrol/src/keycontrol.Domain/Entities/Entity.cs
+++ keycontrol/src/keycontrol.Domain/Entities/Entity.cs
@@ -3,7 +3,8 @@ namespace keycontrol.Domain.Entities;
 public abstract class Entity
 {
     public int Id { get; protected set; }
-    public Guid ExternalId { get; protected set; } = Guid.NewGuid();
+    public Guid ExternalId { get; protected set; } = Guid.Parse("9e4f49fe-0786-44c6-9061-53d2aa84fab3");
     public DateTime CreatedAt { get; protected set; }
     public DateTime UpdatedAt { get; protected set; }
+    
 }
