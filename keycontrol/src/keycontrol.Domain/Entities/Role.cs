f57667004b6b0621c573d1be9d5274ea2502d39b
Erick bueno <erickjb93@gmail.com>
2025-01-15 11:06:57 -0300

    feat: add role

--- keycontrol/src/keycontrol.Domain/Entities/Role.cs
+++ keycontrol/src/keycontrol.Domain/Entities/Role.cs
@@ -1,7 +1,17 @@
-﻿namespace keycontrol.Domain.Entities
+﻿using keycontrol.Domain.Enums;
+using System.Collections.ObjectModel;
+
+namespace keycontrol.Domain.Entities;
+
+public sealed class Role : Enumeration<Role>
 {
-    public class Role
+    public static readonly Role Doorman = new Role(1, "Doorman");
+    public static readonly Role CommonUser = new Role(2, "CommonUser");
+
+    public Role(int id, string name) : base(id, name)
     {
-        
     }
+
+    public ICollection<Permission>? Permissions { get; }
+    public ICollection<User>? Users { get; }
 }
