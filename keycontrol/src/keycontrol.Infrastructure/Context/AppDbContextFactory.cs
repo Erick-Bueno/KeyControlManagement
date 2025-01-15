a5c783c35105858fe34739937d70e25d39ff6448
Erick bueno <erickjb93@gmail.com>
2025-01-15 11:23:15 -0300

    improvment: update app db context factory

--- keycontrol/src/keycontrol.Infrastructure/Context/AppDbContextFactory.cs
+++ keycontrol/src/keycontrol.Infrastructure/Context/AppDbContextFactory.cs
@@ -17,6 +17,6 @@ public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
         var connectionString = configuration.GetConnectionString("Default");
         var options = new DbContextOptionsBuilder<AppDbContext>();
         options.UseNpgsql(connectionString, p => p.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName));
-        return new AppDbContext(options.Options);
+        return new AppDbContext(options.Options, configuration);
     }
 }
