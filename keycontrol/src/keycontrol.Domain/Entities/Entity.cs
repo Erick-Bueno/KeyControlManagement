37009dd73be4b6adb262ce39e6a417879b168787
Erick bueno <erickjb93@gmail.com>
2025-01-15 17:38:29 -0300

    feat: update entity

--- keycontrol/src/keycontrol.Domain/Entities/Entity.cs
+++ keycontrol/src/keycontrol.Domain/Entities/Entity.cs
@@ -4,7 +4,7 @@ public abstract class Entity
 {
     public int Id { get; protected set; }
     public Guid ExternalId { get; protected set; } = Guid.Parse("9e4f49fe-0786-44c6-9061-53d2aa84fab3");
-    public DateTime CreatedAt { get; protected set; }
+    public DateTime CreatedAt { get; protected set; } = DateTime.UtcNow;
     public DateTime UpdatedAt { get; protected set; }
     
 }
--- keycontrol/src/keycontrol.Domain/Entities/Permission.cs
+++ keycontrol/src/keycontrol.Domain/Entities/Permission.cs
@@ -6,5 +6,8 @@ public class Permission
 {
     public int Id { get; set; }
     public string Name { get; init; } = string.Empty;
+    public Guid ExternalId { get; protected set; } = Guid.Parse("9e4f49fe-0786-44c6-9061-53d2aa84fab3");
+    public DateTime CreatedAt { get; protected set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; protected set; }
 
 }
