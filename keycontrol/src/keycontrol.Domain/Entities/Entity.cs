a34b57ee1f80940c87cf7524fe8f4633d7edb29c
Erick bueno <erickjb93@gmail.com>
2025-01-17 17:02:22 -0300

    style: change fields protected to private

--- keycontrol/src/keycontrol.Domain/Entities/Entity.cs
+++ keycontrol/src/keycontrol.Domain/Entities/Entity.cs
@@ -1,10 +1,12 @@
+using keycontrol.Domain.ValueObjects;
+
 namespace keycontrol.Domain.Entities;
 
 public abstract class Entity
 {
-    public int Id { get; protected set; }
-    public Guid ExternalId { get; protected set; } = Guid.Parse("9e4f49fe-0786-44c6-9061-53d2aa84fab3");
-    public DateTime CreatedAt { get; protected set; } = DateTime.UtcNow;
-    public DateTime UpdatedAt { get; protected set; }
+    public int Id { get; private set; }
+    public Guid ExternalId { get; private set; } = Guid.NewGuid();
+    public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; private set; }
     
 }
