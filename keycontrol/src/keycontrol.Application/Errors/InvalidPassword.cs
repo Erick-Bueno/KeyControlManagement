9ed5457fb43638206cba9e36dbc2c5568f8e3d5f
Erick bueno <erickjb93@gmail.com>
2025-01-17 17:01:00 -0300

    refact: move invalid password error to other folder

--- keycontrol/src/keycontrol.Application/Errors/InvalidPassword.cs
+++
@@ -1,5 +0,0 @@
-using keycontrol.Domain.Enums;
-
-namespace keycontrol.Application.Errors;
-
-public record InvalidPassword(string Detail) : AppError(Detail, nameof(InvalidPassword), TypeError.Conflict.ToString());
