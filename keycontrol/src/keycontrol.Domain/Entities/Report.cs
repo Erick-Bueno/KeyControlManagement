bfdd5e2b602f8ea3eb32d1a0af2e5b0a9d885b13
Erick bueno <erickjb93@gmail.com>
2025-01-17 17:02:39 -0300

    feat: add create method in entity

--- keycontrol/src/keycontrol.Domain/Entities/Report.cs
+++ keycontrol/src/keycontrol.Domain/Entities/Report.cs
@@ -1,18 +1,26 @@
 using keycontrol.Domain.Enums;
+using keycontrol.Domain.ValueObjects;
 
 namespace keycontrol.Domain.Entities;
 
 public class Report : Entity
 {
-    public int IdUser { get; }
-    public int IdKey { get; }
-    public Status Status { get; }
-    public DateTime WithdrawalDate { get; }
-    public DateTime? ReturnDate { get; }
-    public required KeyRoom Key { get; set; }
-    public required User User { get; set; }
+    public int IdUser { get; private set;}
+    public int IdKey { get; private set;}
+    public Status Status { get; private set;}
+    public DateTime WithdrawalDate { get; private set;}
+    public DateTime? ReturnDate { get; private set;}
+    public  KeyRoom Key { get;}
+    public  User User { get;}
     public Report()
     {
         Status = Status.Available;
+    }
+    public static Report Create(User user, KeyRoom key){
+        var report = new Report{
+            IdKey = key.Id,
+            IdUser = user.Id,
+        };
+        return report;
     }
 }
