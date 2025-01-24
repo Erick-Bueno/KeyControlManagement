9b4d1cdfee40d55eab650e30615c1434d9253579
Erick bueno <erickjb93@gmail.com>
2025-01-24 10:04:51 -0300

    Test: add entity token tests

---
+++ keycontrol/src/keycontrol.Tests/keycontrol.Domain.Tests/Entities/TokenTests.cs
@@ -0,0 +1,35 @@
+using Bogus;
+using FluentAssertions;
+using keycontrol.Domain.Entities;
+using keycontrol.Domain.ValueObjects;
+using Xunit;
+
+namespace keycontrol.Tests.Keycontrol.Domain.Tests.Entities;
+
+public class TokenTests
+{
+    private readonly Faker _faker = new Faker("pt_BR");
+
+    [Fact]
+    public void Create_GivenTokensData_ThenShouldCreateToken()
+    {
+        var email = Email.Create(_faker.Internet.Email());
+        var refreshToken = _faker.Random.String2(10);
+
+        var result = Token.Create(email.Value, refreshToken);
+
+        result.Email.EmailValue.Should().Be(email.Value.EmailValue);
+        result.RefreshToken.Should().Be(refreshToken);
+    }
+    [Fact]
+    public void Update_GivenTokensData_ThenShouldUpdateToken()
+    {
+        var token = Token.Create(Email.Create(_faker.Internet.Email()).Value, _faker.Random.String2(10));
+        var refreshToken = _faker.Random.String2(10);
+
+        var result = Token.Update(token, refreshToken);
+
+        result.RefreshToken.Should().Be(refreshToken);
+        result.Email.EmailValue.Should().Be(token.Email.EmailValue);
+    }
+}
