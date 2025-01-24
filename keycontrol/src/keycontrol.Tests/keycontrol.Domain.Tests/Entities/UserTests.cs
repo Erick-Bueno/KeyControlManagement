1ec488856218c5a011216f1b56a1e4c7300bb89e
Erick bueno <erickjb93@gmail.com>
2025-01-24 09:12:17 -0300

    Test: add entity user tests

--- keycontrol/src/keycontrol.Tests/keycontrol.Domain.Tests/Entities/UserTests.cs
+++ keycontrol/src/keycontrol.Tests/keycontrol.Domain.Tests/Entities/UserTests.cs
@@ -33,4 +33,18 @@ public class UserTests
         result.IsFailure.Should().BeTrue();
         result.ErrorMessage.Should().Be("Inform an name");
     }
+    [Fact]
+    public void Create_GivenUserValidData_ThenShouldReturnUser()
+    {
+        var name = _faker.Person.FullName;
+        var email = Email.Create(_faker.Person.Email).Value;
+        var password = _faker.Random.AlphaNumeric(8);
+
+        var result = User.Create(name, email, password);
+
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Name.Should().Be(name);
+        result.Value.Email.Should().Be(email);
+        result.Value.Password.Should().Be(password);
+    }
 }
