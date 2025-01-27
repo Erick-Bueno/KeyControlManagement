132676c1793af09fc439ccaab323a7533f2109f8
Erick bueno <erickjb93@gmail.com>
2025-01-27 09:05:07 -0300

    Test: add entity keyroom tests

---
+++ keycontrol/src/keycontrol.Tests/keycontrol.Domain.Tests/Entities/KeyRoomTests.cs
@@ -0,0 +1,39 @@
+using Bogus;
+using FluentAssertions;
+using keycontrol.Domain.Entities;
+using Xunit;
+
+namespace keycontrol.Tests.Entities;
+[Trait("Category", "Entities")]
+public class KeyRoomTests
+{
+    private readonly Faker _faker = new Faker("pt_BR");
+    [Fact]
+    public void Create_GivenDescriptionNullOrEmpty_ThenReturnError ()
+    {
+        var idRoom = _faker.Random.Int();
+        var description = string.Empty;
+        var result = KeyRoom.Create(idRoom, description);
+        result.ErrorMessage.Should().Be("Inform an description");
+        result.IsFailure.Should().BeTrue();
+    }
+    [Fact]
+    public void Create_GivenDescriptionNullOrWhiteSpace_ThenReturnError ()
+    {
+        var idRoom = _faker.Random.Int();
+        var description = " ";
+        var result = KeyRoom.Create(idRoom, description);
+        result.ErrorMessage.Should().Be("Inform an description");
+        result.IsFailure.Should().BeTrue();
+    }
+    [Fact]
+    public void Create_GivenValidKeyRoomsData_ThenCreateKeyRoom ()
+    {
+        var idRoom = _faker.Random.Int();
+        var description = _faker.Random.String2(10);
+        var result = KeyRoom.Create(idRoom, description);
+        result.IsSuccess.Should().BeTrue();
+        result.Value.IdRoom.Should().Be(idRoom);
+        result.Value.Description.Should().Be(description);
+    }
+}
