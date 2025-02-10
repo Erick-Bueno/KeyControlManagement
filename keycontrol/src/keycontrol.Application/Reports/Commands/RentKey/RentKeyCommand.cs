450c5db53871f810eb8aa48514ea7abf20006e30
Erick Bueno <erickjb93@gmail.com>
2025-02-10 13:07:11 -0300

    feat: add rent key command

--- keycontrol/src/keycontrol.Application/Reports/Commands/RentKey/RentKeyCommand.cs
+++ keycontrol/src/keycontrol.Application/Reports/Commands/RentKey/RentKeyCommand.cs
@@ -1,3 +1,8 @@
+using keycontrol.Application.Errors;
+using keycontrol.Application.Reports.Responses;
+using MediatR;
+using OneOf;
+
 namespace keycontrol.Application.Reports.Commands.RentKey;
 
-public record RentKeyCommand(int IdUser, int IdKey) : 
+public record RentKeyCommand(Guid ExternalIdUser, Guid ExternalIdKey) : IRequest<OneOf<RentKeyResponse, AppError>>;
