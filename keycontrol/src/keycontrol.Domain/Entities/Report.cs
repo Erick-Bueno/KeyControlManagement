d48e1a96cc32741be93dd14461fe2d05ee80bc85
Erick Bueno <erickjb93@gmail.com>
2025-02-10 13:12:50 -0300

    🔥 style: remove status prop

--- keycontrol/src/keycontrol.Domain/Entities/Report.cs
+++ keycontrol/src/keycontrol.Domain/Entities/Report.cs
@@ -8,14 +8,13 @@ public class Report : Entity
 {
     public int IdUser { get; private set;}
     public int IdKey { get; private set;}
-    public Status Status { get; private set;}
     public DateTime WithdrawalDate { get; private set;}
     public DateTime? ReturnDate { get; private set;}
     public  KeyRoom Key { get;}
     public  User User { get;}
     private Report()
     {
-        Status = Status.Unavailable;
+
     }
     public static Result<Report> Create(User user, KeyRoom key){
         var report = new Report{
