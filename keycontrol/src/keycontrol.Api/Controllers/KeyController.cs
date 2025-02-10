29c27f7fd60f6951d841960de3d9d74ba5d0252f
Erick Bueno <erickjb93@gmail.com>
2025-02-10 13:05:45 -0300

    feat: add rent key endpoint

--- keycontrol/src/keycontrol.Api/Controllers/KeyController.cs
+++ keycontrol/src/keycontrol.Api/Controllers/KeyController.cs
@@ -1,10 +1,11 @@
 ﻿using keycontrol.Application.Extension;
 using keycontrol.Application.Keys.Commands.RegisterKey;
 using keycontrol.Application.Keys.Requests;
+using keycontrol.Application.Reports.Commands.RentKey;
+using keycontrol.Application.Reports.Requests;
 using keycontrol.Domain.Enums;
 using keycontrol.Infrastructure.Authentication;
 using MediatR;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace keycontrol.Api.Controllers;
@@ -15,12 +16,19 @@ public class KeyController : ApiController
     {
         _sender = sender;
     }
-    [HasPermission(Permission.Administrator)]  
+    [HasPermission(Permission.Administrator)]
     [HttpPost]
     public async Task<IActionResult> AddKey([FromBody] RegisterKeyRequest registerKeyRequest)
     {
         var registerKeyCommand = new RegisterKeyCommand(registerKeyRequest.ExternalIdRoom, registerKeyRequest.Description);
         var result = await _sender.Send(registerKeyCommand);
         return this.RegisterKeyResponseBase(result);
+    }
+    [HttpPost]
+    public async Task<IActionResult> RentKey([FromBody] RentKeyRequest rentKeyRequest)
+    {
+        var rentKeyCommand = new RentKeyCommand(rentKeyRequest.ExternalIdUser, rentKeyRequest.ExternalIdKey);
+        var result = await _sender.Send(rentKeyCommand);
+        return this.RentKeyResponseBase(result);
     }
 }
