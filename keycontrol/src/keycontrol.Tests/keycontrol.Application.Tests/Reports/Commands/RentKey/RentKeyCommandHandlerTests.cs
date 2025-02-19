c45fa6a794edde66027edc9384721700426b0d4c
Erick Bueno <erickjb93@gmail.com>
2025-02-18 22:35:48 -0300

    🧪  test: add test to rent key command handler

--- keycontrol/src/keycontrol.Tests/keycontrol.Application.Tests/Reports/Commands/RentKey/RentKeyCommandHandlerTests.cs
+++ keycontrol/src/keycontrol.Tests/keycontrol.Application.Tests/Reports/Commands/RentKey/RentKeyCommandHandlerTests.cs
@@ -2,6 +2,7 @@ using Bogus;
 using FluentAssertions;
 using keycontrol.Application.Errors;
 using keycontrol.Application.Reports.Commands.RentKey;
+using keycontrol.Application.Reports.Responses;
 using keycontrol.Application.Repositories;
 using keycontrol.Domain.Entities;
 using keycontrol.Domain.ValueObjects;
@@ -65,7 +66,7 @@ public class RentKeyCommandHandlerTests
 
     }
     [Fact]
-    public async Task Handle_GivenUnavailableKey_ThenReturnKeyUnavailableErrorAsync()
+    public async Task Handle_GivenNotRegisteredKey_ThenReturnKeyNotFoundErrorAsync()
     {
         var externalIdUser = _faker.Random.Guid();
         var externalIdKey = _faker.Random.Guid();
@@ -78,10 +79,58 @@ public class RentKeyCommandHandlerTests
 
         var result = await _rentKeyCommandHandler.Handle(rentKeyCommand, CancellationToken.None);
 
-        var expectedResponseError = new UserBlocked("This Key Is Unavailable");
+        var expectedResponseError = new KeyNotFound("Key not found");
 
         result.AsT1.Detail.Should().Be(expectedResponseError.Detail);
         result.AsT1.ErrorType.Should().Be(expectedResponseError.ErrorType);
         result.AsT1.NameError.Should().Be(expectedResponseError.NameError);
+    }
+    [Fact]
+    public async Task Handle_GivenUnavailableKey_ThenReturnUnavailableKeyErrorAsync()
+    {
+        var externalIdUser = _faker.Random.Guid();
+        var externalIdKey = _faker.Random.Guid();
+        var rentKeyCommand = new RentKeyCommand(externalIdUser, externalIdKey);
+        var email = Email.Create(_faker.Person.Email);
+        var user = User.Create(_faker.Person.FullName, email.Value, ValidPassword.Generate());
+        var keyRoom = KeyRoom.Create(_faker.Random.Int(), _faker.Lorem.Text());
+        keyRoom.SetStatus();
+
+        _userRepositoryMock.Setup(ur => ur.FindUserByExternalId(externalIdUser)).ReturnsAsync(user.Value);
+        _keyRepositoryMock.Setup(kr => kr.FindKeyByExternalId(externalIdKey)).ReturnsAsync(keyRoom.Value);
+
+        var result = await _rentKeyCommandHandler.Handle(rentKeyCommand, CancellationToken.None);
+
+        var expectedResponseError = new KeyUnavailable("This Key Is Unavailable");
+
+        result.AsT1.Detail.Should().Be(expectedResponseError.Detail);
+        result.AsT1.ErrorType.Should().Be(expectedResponseError.ErrorType);
+        result.AsT1.NameError.Should().Be(expectedResponseError.NameError);
+
+
+    }
+    [Fact]
+    public async Task Handle_GivenValidRentKeyData_ThenCreateReportAsync()
+    {
+        var externalIdUser = _faker.Random.Guid();
+        var externalIdKey = _faker.Random.Guid();
+        var rentKeyCommand = new RentKeyCommand(externalIdUser, externalIdKey);
+        var email = Email.Create(_faker.Person.Email);
+        var user = User.Create(_faker.Person.FullName, email.Value, ValidPassword.Generate());
+        var keyRoom = KeyRoom.Create(_faker.Random.Int(), _faker.Lorem.Text());
+        var report = Report.Create(user.Value, keyRoom.Value);
+        _userRepositoryMock.Setup(ur => ur.FindUserByExternalId(externalIdUser)).ReturnsAsync(user.Value);
+        _keyRepositoryMock.Setup(kr => kr.FindKeyByExternalId(externalIdKey)).ReturnsAsync(keyRoom.Value);
+        _reportRepositoryMock.Setup(rr => rr.AddReport(report.Value));
+
+        var result = await _rentKeyCommandHandler.Handle(rentKeyCommand, CancellationToken.None);
+
+        var expectedResponseSuccess = new RentKeyResponse(user.Value.ExternalId, keyRoom.Value.ExternalId, user.Value.Name, report.Value.WithdrawalDate);
+
+        result.AsT0.ExternalIdKey.Should().Be(expectedResponseSuccess.ExternalIdKey);
+        result.AsT0.Name.Should().Be(expectedResponseSuccess.Name);
+        result.AsT0.WithdrawalDate.Should().Be(expectedResponseSuccess.WithdrawalDate);
+        result.AsT0.Message.Should().Be("Key rented successfully");
+        result.AsT0.ExternalIdUser.Should().Be(expectedResponseSuccess.ExternalIdUser);
     }
 }
