129e6476cfb3cca292e9e969773914f90f383390
Erick bueno <erickjb93@gmail.com>
2025-02-21 17:12:35 -0300

    ✨ feat : add key room update status method

--- keycontrol/src/keycontrol.Domain/Entities/KeyRoom.cs
+++ keycontrol/src/keycontrol.Domain/Entities/KeyRoom.cs
@@ -6,28 +6,36 @@ using System.Diagnostics.Contracts;
 namespace keycontrol.Domain.Entities;
 
 public class KeyRoom : Entity
-{   
+{
     public int IdRoom { get; private set; }
     public string? Description { get; private set; }
     public Status Status { get; private set; }
     public Room? Room { get; }
     public ICollection<Report>? Reports { get; }
-    
+
     private KeyRoom(int idRoom, string? description)
     {
-        IdRoom = idRoom; 
+        IdRoom = idRoom;
         Description = description;
         Status = Status.Available;
     }
-    public KeyRoom(){}
-    public static Result<KeyRoom> Create(int idRoom, string description){
-        if(string.IsNullOrEmpty(description)){
+    public KeyRoom() { }
+    public static Result<KeyRoom> Create(int idRoom, string description)
+    {
+        if (string.IsNullOrEmpty(description))
+        {
             return Result<KeyRoom>.Failure("Inform an description");
         }
-        if(string.IsNullOrWhiteSpace(description)){
+        if (string.IsNullOrWhiteSpace(description))
+        {
             return Result<KeyRoom>.Failure("Inform an description");
-        } 
+        }
         return Result<KeyRoom>.Success(new KeyRoom(idRoom, description));
+    }
+
+    public void UpdateStatus(Status newStatus)
+    {
+        Status = newStatus;
     }
 
 }
