eb7230c2c862fcf67a8506f2e7c06e0579be4a62
Erick bueno <erickjb93@gmail.com>
2025-02-21 21:59:17 -0300

    ✨ feat : add find by external id method

--- keycontrol/src/keycontrol.Application/Repositories/IReportRepository.cs
+++ keycontrol/src/keycontrol.Application/Repositories/IReportRepository.cs
@@ -3,4 +3,5 @@ namespace keycontrol.Application.Repositories;
 public interface IReportRepository
 {
     public Task<Report> AddReport(Report report);
+    public Task<Report> FindReportByExternalId(Guid externalId);
 }
